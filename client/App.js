3b3b31ff7c71e76a46e06fd8ec7b8b603c208c93
Johnny Nguyen <98909274+ninjaX172@users.noreply.github.com>
2023-02-17 14:40:31 -0600

    Add files via upload

---
+++ client/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import LoginPage from './LoginPage';
+import Home from './Home';
+function App() {
+ 
+
+  return (
+    
+    <div className="App">
+      
+
+
+      <LoginPage/>
+      {/* <Home/> */}
+      
+      
+      </div>
+  );
+}
+
+export default App;
