3eabc09fc0fd0f035ddf29cd96f2ae7485f36f51
Darian Tenio <99412647+D-Tenio@users.noreply.github.com>
2023-02-22 13:32:59 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,9 +18,10 @@ function App() {
       {/* <RegisterPage/> */}
       {/* <Home/>  */}
       <Routes>
-        <Route path="/" element={<RegisterPage/>}/>
-        <Route path="/Login" element={<LoginPage/>}/>
+        
+        <Route path="/" element={<LoginForm/>}/>
         <Route path='/home' element={<Home/>}></Route>
+        <Route path='/SignUp' element={<SignUp/>}/>
         <Route path="/Quota" element={<Quota/>}/>
         <Route path="/History" element={<History/>}/>
         <Route path="/Profile" element={<Profile/>}/>
