79167a8ffe8be79bb8ba5729f5b8d45211099c0d
Darian Tenio <99412647+D-Tenio@users.noreply.github.com>
2023-03-23 22:33:47 -0500

    Added backend to register page

--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -1,17 +1,49 @@
 import {Link} from "react-router-dom";
+import React, {useState} from "react";
+import axios from "axios"
 import "./SignUp.css";
 
+
+
+
 const SignUp = () => {
+
+
+    const [usernameReg, setUsernameReg] = useState('')
+    const [passwordReg, setPasswordReg] = useState('')
+
+    const register = () => {
+        axios.post('http://localhost:3001/register', {
+            username: usernameReg, 
+            password: passwordReg,
+        }).then((response) => {
+            console.log(response);
+        });
+    };
+
+
     return(
         <section className='section'>
             <p className="title">Sign Up Page</p>
             <div className="cover">
                 <h1>Sign Up form</h1>
-                <input type="text" placeholder="Create username" />
-                <input type="password1" placeholder="Enter password" />
-                <input type="password2" placeholder="Re-enter password" />
+                <input 
+                    type="text" 
+                    onChange={(e) => {
+                        setUsernameReg(e.target.value);
+                    }}
+                />
 
-                <div className="signup-btn">Sign Up</div>
+                <input 
+                    type="password" 
+                    onChange={(e) => {
+                        setPasswordReg(e.target.value);
+                    }}
+                />
+
+                <button onClick={register}>Sign Up</button>
+
+
                 <Link to='/' className='btn'>
                     Already have an account? Log in!
                 </Link>
