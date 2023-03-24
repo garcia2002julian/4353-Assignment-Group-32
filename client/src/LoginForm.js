e92f83aa502d5146ccfd4c440428de8a56c3f6de
garcia2002julian <garcia2002julian@gmail.com>
2023-03-24 13:53:33 -0500

    Node modules

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,79 +1,70 @@
-import {Link, useNavigate} from "react-router-dom";
-import React, {useState} from "react";
-import axios from "axios"
-import {useAuth} from "./Auth";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
+import { useAuth } from "./Auth";
 import "./LoginForm.css";
 
 const LoginForm = () => {
-    
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const auth = useAuth();
-    const navigate = useNavigate();
-    
-    const [loginStatus, setloginStatus] = useState('');
+  const auth = useAuth();
+  const navigate = useNavigate();
 
+  const [loginStatus, setloginStatus] = useState("");
 
-    const login = () => {
-        axios.post('http://localhost:3001/login', {
-            username: username, 
-            password: password,
-        }).then((response) => {
+  const login = () => {
+    axios
+      .post("http://localhost:3001/login", {
+        username: username,
+        password: password,
+      })
+      .then((response) => {
+        if (response.data.message) {
+          setloginStatus(response.data.message);
+        } else {
+          axios
+            .get("http://localhost:3001/getUserInfo/${username}", {})
+            .then((response) => {
+              //console.log(response.data[0].password);
+              auth.login_(username);
+              navigate("/Profile");
+            });
+        }
+        console.log(response.data);
+      });
+  };
 
-            if(response.data.message){
-                setloginStatus(response.data.message)
-            } else {
-                axios
-                .get('http://localhost:3001/getUserInfo/${username}', {})
-                .then((response) => {
-                    //console.log(response.data[0].password);
-                    auth.login_(username);
-                    navigate("/Profile");
-                });
-            }
-            console.log(response.data);
-        });
-    };
+  return (
+    <section className="section">
+      <p class="header">Login Page</p>
+      <div className="cover">
+        <h1>Log In</h1>
 
-    
-    return (
-        <section className='section'>
-            <p class='header'>Login Page</p>
-            <div className="cover">
-                
-                <h1>Log In</h1>
+        <input
+          type="text"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
 
-                <input 
-                    type="text" 
-                    onChange={(e) => {
-                        setUsername(e.target.value);
-                    }}
-                />
+        <input
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
 
-                <input 
-                    type="password" 
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                    }}
-                />
+        <button onClick={login}>Log In</button>
 
-                <button onClick={login}>Log In</button>
-                
+        <Link to="/signup" className="btn">
+          Don't have an account? Sign up here!
+        </Link>
 
-                <Link to='/signup' className='btn'>
-                    Don't have an account? Sign up here!
-                </Link>
-                
-                <h1>{loginStatus}</h1>
-            </div>
-            
-        </section>
-
-            
-    );
+        <h1>{loginStatus}</h1>
+      </div>
+    </section>
+  );
 };
 
-
-export default LoginForm 
-//
+export default LoginForm;
